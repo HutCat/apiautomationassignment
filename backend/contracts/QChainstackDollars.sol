4de3fbe93e8e44197e7c96aa56097824abfdab88
HutCat <118656992+HutCat@users.noreply.github.com>
2020-07-11 15:55:47 +0000

    updating read me

--- backend/contracts/QChainstackDollars.sol
+++ backend/contracts/QChainstackDollars.sol
@@ -4,4 +4,14 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 // Import ERC20
-import 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract QChainstackDollars is ERC20, ERC20Burnable {
+    address bridge;
+
+    event TokensMinted(address from, uint256 amount);
+
+    constructor(address _bridge) ERC20("QChainstackDollars", "Q-CHSD") {
+        bridge = _bridge;
+        consol
