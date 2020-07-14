9619eae9a8d5e7f990baf338375ef5077ff738a7
HutCat <118656992+HutCat@users.noreply.github.com>
2020-07-14 05:59:41 +0000

    Minor fixed

--- backend/event-watcher.js
+++ backend/event-watcher.js
@@ -11,3 +11,13 @@ const {
 } = require('./contract-methods.js')
 
 const ORIGIN_TOKEN_CONTRACT_ADDRESS = process.env.ORIGIN_TOKEN_CONTRACT_ADDRESS
+const DESTINATION_TOKEN_CONTRACT_ADDRESS =
+  process.env.DESTINATION_TOKEN_CONTRACT_ADDRESS
+const BRIDGE_WALLET = process.env.BRIDGE_WALLET
+
+const BRIDGE_WALLET_KEY = process.env.BRIDGE_PRIV_KEY
+
+const CHSD_ABIJSON = require('./ChainstackDollars.json')
+const QCHSD_ABIJSON = require('./DChainstackDollars.json')
+
+const handleEthEvent = async (even
