3703878502594165e55744f8867ae262abf76d9f
HutCat <118656992+HutCat@users.noreply.github.com>
2020-08-11 06:37:32 +0000

    quickfix

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -20,4 +20,22 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0
+  solidity: '0.8.4',
+  paths: {
+    sources: './contracts',
+    artifacts: '../web/src/artifacts',
+    tests: './test',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    ganache: {
+      chainId: 5777,
+      url: 'http://127.0.0.1:7545',
+    },
+    origin: {
+      url: process.env.DEPLOY_ENDPOINT_ORIGIN,
+      accounts: [process.env.DEPLOY_ACC_KEY],
+    },
+    d
