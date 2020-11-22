4933ebc9f60d9e5b65647f3b06f4c354c249b6b0
HutCat <118656992+HutCat@users.noreply.github.com>
2020-11-22 23:04:12 +0000

    cambios en el plugin usuarios

--- solidity/scripts/deployDestination.js
+++ solidity/scripts/deployDestination.js
@@ -6,4 +6,20 @@ const main = async () => {
   const accountBalance = await deployer.getBalance()
 
   console.log('Deploying contracts with account: ', deployer.address)
-  console.log('Account balance: '
+  console.log('Account balance: ', accountBalance.toString())
+
+  let contractFactory = await hre.ethers.getContractFactory(
+    'DChainstackDollars'
+  )
+  let contract = await contractFactory.deploy(process.env.BRIDGE_WALLET)
+
+  await contract.deployed()
+
+  console.log(
+    'contract DChainstackDollars deployed to address: ',
+    contract.address
+  )
+}
+
+const runMain = async () => {
+  try {
