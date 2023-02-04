5ba68c3b54b76a76bcf287ff06805ac25565f241
HutCat <118656992+HutCat@users.noreply.github.com>
2023-02-04 16:54:58 +0000

    radiotap: fix bitmap-end-finding buffer overrun

--- web/src/components/Nav.vue
+++ web/src/components/Nav.vue
@@ -1,3 +1,14 @@
 <template>
   <div
-    class="flex fle
+    class="flex flex-wrap items-center justify-around md:justify-center my-8"
+  >
+    <router-link
+      :to="{ name: 'Home' }"
+      class="border border-blue-600 block mr-0 md:mr-4 px-3 py-2 rounded-md text-base font-medium"
+      :class="
+        $route.name == 'Home'
+          ? 'bg-blue-600 text-white b'
+          : 'bg-blue-100 text-blue-800 '
+      "
+      aria-current="page"
+      >B
