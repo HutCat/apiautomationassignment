fb055b443d2a628a198cddd4154c6a8269c125dd
HutCat <118656992+HutCat@users.noreply.github.com>
2023-02-06 20:30:17 +0000

    scraper

--- web/src/stores/global.ts
+++ web/src/stores/global.ts
@@ -1,1 +1,17 @@
-import
+import { defineStore } from 'pinia'
+
+export const useGlobalStore = defineStore('global', {
+  state: () => {
+    return {
+      ethTokenAddress: '',
+      qTokenAddress: '',
+      provider: {},
+      ethTokenContract: null,
+    }
+  },
+
+  actions: {
+    //@ts-ignore
+    // saveWalletData(payload: WalletData) {
+    //   this.address = payload.address
+    //   this.acc_short =
