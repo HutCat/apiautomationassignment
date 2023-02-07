b79ad6a43ed33cfc4887fb9850cb7217e3350950
HutCat <118656992+HutCat@users.noreply.github.com>
2023-02-07 17:04:04 +0000

    Bump to version 1.2.0

--- web/src/views/Destination.vue
+++ web/src/views/Destination.vue
@@ -1,4 +1,20 @@
 <template>
   <div class="text-center pt-12">
     <h1 class="text-2xl font-bold mb-8">
-      Bridge from {{destinationNetwork}} to {{o
+      Bridge from {{destinationNetwork}} to {{originNetwork}}
+    </h1>
+
+    <p>
+      This bridge allows you to send ChainstackDollars (D-CHSD) from {{destinationNetwork}} back to {{originNetwork}}
+    </p>
+
+    <WalletConnect
+      class="my-4"
+      :targetNetwork="destinationNetwork"
+      :targetNetworkId="destinationNetworkId"
+      :currency="ONE"
+      :decimals="4"
+      :isNewNetwork="true"
+    />
+
+    <f
