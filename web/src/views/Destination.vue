220c3457a145ab7aae79ec3d32450874d3d16c92
HutCat <118656992+HutCat@users.noreply.github.com>
2023-02-08 11:08:08 +0000

    Work

--- web/src/views/Destination.vue
+++ web/src/views/Destination.vue
@@ -24,4 +24,15 @@
       <div class="mt-4 w-2/3 mx-auto relative rounded-md shadow-sm">
         <div
           class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
-        
+        >
+          <span class="text-gray-500 sm:text-sm"> $ </span>
+        </div>
+        <input
+          type="text"
+          v-model="amount"
+          name="price"
+          id="price"
+          class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+          placeholder="0.00"
+          aria-describedby="price-currency"
+  
