b1a1a7f0ac506523d0adb9829775e364d3f8c7d2
HutCat <118656992+HutCat@users.noreply.github.com>
2023-02-09 16:55:21 +0000

    Updating dist files

--- web/src/views/Destination.vue
+++ web/src/views/Destination.vue
@@ -62,4 +62,18 @@
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2
+            d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
+          />
+        </svg>
+        {{ trxInProgress ? `Processing...` : `Bridge to ${originNetwork}` }}
+      </button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { ethers, BigNumber } from 'ethers'
+
+import { useWalletStore } from '../stores/wallet'
+impo
