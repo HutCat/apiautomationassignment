c814f4a24fb6236f6392b6d0558bfbbd1d3e83a8
HutCat <118656992+HutCat@users.noreply.github.com>
2023-02-09 16:45:11 +0000

    .ignore

--- web/src/views/Destination.vue
+++ web/src/views/Destination.vue
@@ -53,4 +53,13 @@
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          cl
+          class="m-ml-1 mr-3 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2
