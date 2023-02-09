29b4ee454476c8135fd4882a33b93b81afa61984
HutCat <118656992+HutCat@users.noreply.github.com>
2023-02-09 12:34:00 +0000

    af9005: fix kernel panic on init if compiled without IR

--- web/src/views/Destination.vue
+++ web/src/views/Destination.vue
@@ -35,4 +35,16 @@
           class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
           placeholder="0.00"
           aria-describedby="price-currency"
-  
+        />
+        <div
+          class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none"
+        >
+          <span class="text-gray-500 sm:text-sm" id="price-currency">
+            D-CHSD
+          </span>
+        </div>
+      </div>
+      <p class="text-xs mt-1">Your balance is: {{ walletBalance }}</p>
+
+      <button
+        type="
